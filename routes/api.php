5a0ea979bdbeda44a1828bd633e18ee6a824cb02
prilandini <prillyandininurul@gmail.com>
2018-01-10 13:10:19 +0700

    make some endpoints again

--- routes/api.php
+++ routes/api.php
@@ -24,9 +24,15 @@ Route::group(['middleware' => 'auth:api'],function(){
     Route::resource('/albums', 'AlbumController');
     Route::resource('/photos', 'PhotoController');
     Route::resource('/todos', 'TodoController');
+
+    Route::get('/posts/{post}/comments', 'PostController@getComments')->name('Posts.getComments');
+    Route::get('/albums/{albums}/photos', 'AlbumController@getPhotos')->name('Albums.getPhotos');
+    Route::get('/users/{users}/todos', 'UserController@getTodos')->name('Users.getTodos');
+    Route::get('/users/{users}/posts', 'UserController@getPosts')->name('Users.getPosts');
+    Route::get('/users/{users}/albums', 'UserController@getAlbums')->name('Users.getAlbums');
 });
 
-Route::get('/posts/{post}/comments', 'PostController@getComment')->name('Posts.getComment');
+
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
